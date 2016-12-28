e7c820233abe5842d632295cfd524f718c8f15cf
ihven <yapetryk21@gmail.com>
2016-12-28 09:23:12 +0200

    implement sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
+ 
+  devise_for :users 
+   
+
+  root to: "home#index"
   get '/'=> 'home#index'
   get 'home/index' 
   
   get 'about'=>'pages#about'
   get 'terms'=>'pages#terms'
   
-  
   resources :articles  do
            resources :comments
   end
+  
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
